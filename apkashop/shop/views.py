420b3b8473a770b596e873bc7973de03dfd29652
sharadmehra697 <34804575+sharadmehra697@users.noreply.github.com>
2019-10-03 11:43:08 +0530

    Add files via upload

--- apkashop/shop/views.py
+++ apkashop/shop/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse,HttpResponseRedirect
 from shop.models import first
 # Create your views here.
 def HomePage(request):
-	s=first()
-	s.first_name='sharad'
-	s.save()
-	return HttpResponse(s.first_name)
+	
+	return HttpResponse('all set')
