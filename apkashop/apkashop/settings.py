cfdc28a6ac152af6b90cdb0dad71958a015aefa9
sharadmehra697 <34804575+sharadmehra697@users.noreply.github.com>
2019-10-03 12:18:19 +0530

    Add files via upload

--- apkashop/apkashop/settings.py
+++ apkashop/apkashop/settings.py
@@ -77,16 +77,9 @@ WSGI_APPLICATION = 'apkashop.wsgi.application'
 
 DATABASES = {
     'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'apkashop',
-'USER': 'root',
-'PASSWORD': '',
-'HOST': '',
-'PORT': '',
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-}
-}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
--- apkashop/shop/views.py
+++ apkashop/shop/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-from shop.models import first
 # Create your views here.
 def HomePage(request):
 	
